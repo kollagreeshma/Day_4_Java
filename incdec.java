a1771f0c27ceb75175662253abc8137eb5f01c61
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-05 16:02:41 +0530

    Add files via upload

---
+++ incdec.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class incdec{
+    public static void main(String args[]){
+        int n=5;
+        System.out.println(n++);
+        int a=4;
+        System.out.println(++a);
+        int b=6;
+        System.out.println(b--);
+        int c=3;
+        System.out.println(--c);
+    }
+}
