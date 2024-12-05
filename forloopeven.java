6711484c8b49bfe2171ea8b8173b4cd9628e5d85
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-05 13:27:32 +0530

    Add files via upload

---
+++ forloopeven.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ class even{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        for(int i=0;i<=x;i+=2){
+            System.out.println("i="+i);
+        }
+    }
+}
