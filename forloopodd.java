a0b5be3ddd44470e1c493d98af2325dd27e10f3a
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-05 13:27:59 +0530

    Add files via upload

---
+++ forloopodd.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ class odd{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        for(int i=1;i<=x;i+=2){
+            System.out.println("i="+i);
+        }
+    }
+}
