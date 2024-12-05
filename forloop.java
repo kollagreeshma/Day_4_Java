0479e12ef869bbd40d3301ed464005e666704939
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-05 12:41:29 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ class a{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        for(int i=0;i<=x;++i){
+            System.out.println("i="+i);
+        }
+    }
+}
