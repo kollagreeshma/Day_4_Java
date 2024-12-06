ce985aab282852f7960d3be40ef13ce05116c9ff
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-06 10:19:54 +0530

    Add files via upload

---
+++ while.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class b{
+    public static void main(String args[]){
+        int n=10;
+        int a=n-1;
+        while(a>=0){
+            System.out.println(n-a);
+            a-=1;
+        }
+    }
+}
