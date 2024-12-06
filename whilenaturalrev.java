2513ef4e797b9c459c125f4d61a24978fe25dab1
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-06 10:23:46 +0530

    Add files via upload

---
+++ whilenaturalrev.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class c{
+    public static void main(String args[]){
+        int n=100;
+        int i=n;
+        while(i>-1){
+            System.out.println(i);
+            i-=1;
+        }
+    }
+}
